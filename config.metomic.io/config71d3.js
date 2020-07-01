f19da9aaa1851a74770fab48df8b5d16ee0e6249
jeyj99 <55855898+jeyj99@users.noreply.github.com>
2020-07-01 18:06:27 -0400

    Added Basic Files

---
+++ config.metomic.io/config71d3.js
@@ -0,0 +1,8 @@
+
+window._mtm = window._mtm || {};
+window._mtm.projectId = "prj:cdad1720-36f9-4f52-845b-363ddb9d9732";
+window._mtm.configAutoblocking =
+{
+    "rules": [],
+    "autoblockingEnabled": true
+}
